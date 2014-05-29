e9ee045547ae1e6d8059bde23282e49ed2a686d7
hefangshi <hefangshi@gmail.com>
2014-05-29 12:54:48 +0800

    调整插件

--- fis-conf.js
+++ fis-conf.js
@@ -1,5 +1,5 @@
-//Step 1. 取消下面的注释开启combine插件，注意需要先进行插件安装 npm install -g fis-postpackager-combine
-// fis.config.set('modules.postpackager', 'combine');
+//Step 1. 取消下面的注释开启simple插件，注意需要先进行插件安装 npm install -g fis-postpackager-simple
+// fis.config.set('modules.postpackager', 'simple');
 
 //通过pack设置干预自动合并结果，将公用资源合并成一个文件，更加利于页面间的共用
 
@@ -17,8 +17,8 @@
 
 //autoCombine设置是否将零散资源进行合并，关闭此功能则只会自动将页面中的零散资源按pack设置进行合并
 
-//取消下面的注释可以关闭combine对零散资源的自动合并，但是依然会将pack的资源引用自动替换
-// fis.config.set('settings.postpackager.combine.autoCombine', false);
+//取消下面的注释可以关闭simple对零散资源的自动合并，但是依然会将pack的资源引用自动替换
+// fis.config.set('settings.postpackager.simple.autoCombine', false);
 
 
 //Step 3. 取消下面的注释开启图片合并功能
